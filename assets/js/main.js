9e217dcb6a99ae3cb04f7548da8817314a0153d2
doha <147937202+doha-el-grine@users.noreply.github.com>
2024-09-11 14:45:15 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,155 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+
+/*=============== SHOW MENU ===============*/
+/* Validate if constants exist */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+
+/*=============== HIDE MENU ===============*/
+/* Validate if constants exist */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //when we click ON EACH NAV_LINK WE REMOVE TGHE SHOW MENU CALSS
+    navMenu.classList.remove('show-menu')
+}
+    navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // WHEN THE SCROLL IS GREATER THAN 50 view port height
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+    window.addEventListener('scroll', scrollHeader)
+
+
+
+
+/*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween:24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+
+          spaceBetween: 48,
+        },
+      },
+  });
+
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+let mixerFeatured = mixitup('.featured__container', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+
+/* Link active featured */ 
+const LinkFeatured = document.querySelectorAll('.featured__item')
+
+function activeFeatured(){
+    LinkFeatured.forEach(l =>l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+LinkFeatured.forEach(l => l.addEventListener('click',activeFeatured))
+
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+   sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offseTop - 58,
+          sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' +sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' +sectionId + ']').classList.remove('active-link')
+
+        }
+   })
+}
+
+window.addEventListener('scroll' , scrollActive)
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const sr = ScrollReveal({
+      origin: 'top',
+      distance: '60px',
+      duration: 2500,
+      delay: 400,
+    //   reset:true
+    })
+
+    sr.reveal('.home__title, .popular__container, .features__img, .featured__filters');
+    sr.reveal('.home__subtitle', { delay: 500 })
+    sr.reveal('.home__elec', { delay: 600 })
+    sr.reveal('.home__img', { delay: 800 })
+    sr.reveal('.home__car-data', { delay: 900, interval: 100, origin: 'bottom' })
+    sr.reveal('.home__button', { delay: 1000, origin: 'bottom' })
+    sr.reveal('.about__group, .offer__data', {  origin: 'left' })
+    sr.reveal('.about__data,  .offer__img', {  origin: 'right' });
+    sr.reveal('.features__map', { delay:600, origin: 'bottom' });
+    sr.reveal('.features__card', { interval: 300 });
+    sr.reveal('.featured__card, .logos__content,  .footer__content ', { interval: 100 });
+
+
+});
+
+  
+
